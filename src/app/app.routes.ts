f15e3d26ef285a975122d36cc55adbc73812ea69
Cesar Martinez <cesarwamartinez@gmail.com>
2025-04-14 17:54:48 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,11 @@
 import { Routes } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './routes/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: HomeComponent},
+    {path: "footer", component: FooterComponent},
+    {path: "navbar", component: NavbarComponent}
+];
