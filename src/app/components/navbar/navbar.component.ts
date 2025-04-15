84cf2ef6bb1d2afb48a264213e738c071b90a924
Cesar Martinez <cesarwamartinez@gmail.com>
2025-04-15 17:13:35 -0500

    Se añadio la primera vista, pero faltan algunas correciones

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,36 +1,35 @@
 import { Component, ElementRef, viewChild } from '@angular/core';
 import { ViewChild } from '@angular/core';
 
-interface RoutesType{
-  name: string
-  id: string 
+interface RoutesType {
+  name: string;
+  id: string;
 }
+
 
 @Component({
   selector: 'app-navbar',
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
-
 export class NavbarComponent {
-
-  
   routes: RoutesType[] = [
-    { name: "¿Por qué?", id: "porque" },
-    { name: "Nuestros planes", id: "planes" },
-    { name: "Club" , id: "club"},
-    { name: "¿Quieres más info?", id: "moreinfo"  },
-    { name: "Blog", id: "blog" },
-    { name: "Asistencias", id: "asistencias" }
+    { name: '¿Por qué?', id: 'porque' },
+    { name: 'Nuestros planes', id: 'planes' },
+    { name: 'Club', id: 'club' },
+    { name: '¿Quieres más info?', id: 'moreinfo' },
+    { name: 'Blog', id: 'blog' },
+    { name: 'Asistencias', id: 'asistencias' },
   ];
 
-  scrollToWhy(id: string = "porque") {
+
+  
+  scrollToWhy(id: string = 'porque') {
     const el = document.getElementById(id);
-    console.log(el)
+    console.log(el);
     if (el) {
       el.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
   }
-  
 }
--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 
+
+interface FormArray{
+  label: string
+  type: string
+}
+
 @Component({
   selector: 'app-home',
   imports: [NavbarComponent, FooterComponent],
@@ -12,4 +18,18 @@ export class HomeComponent {
   activate: boolean = false
   indexActivate : number | string = 0
 
-}
+
+  formArray: FormArray[] = [
+    { label: "Nombre del peludo", type: "text" },
+    { label: "Edad", type: "number" },
+    { label: "Mes", type: "text" },
+    { label: "Raza del peludito", type: "text" },
+    { label: "Primer nombre", type: "text" },
+    { label: "Segundo Nombre", type: "text" },
+    { label: "Primer apellido", type: "text" },
+    { label: "Segundo apellido", type: "text" },
+    { label: "Tipo de documento", type: "text" },
+    { label: "Numero de documento", type: "number" },
+    { label: "Correo electrónico", type: "email" },
+  ];
+};    
